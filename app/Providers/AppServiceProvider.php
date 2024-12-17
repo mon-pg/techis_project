24c4321584ea096bb4aa0d8a2b63a12817b7761c
mon-pg <178457415+mon-pg@users.noreply.github.com>
2024-12-18 03:17:03 +0900

    httpsセキュアに変更

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,5 +21,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        if(\App::environment(['production'])) {
+            \URL::forseScheme('https');
+        }
     }
 }
